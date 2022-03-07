bf457b41ed1752f4ce057feb3996d13a5953e144
BIIYEUDAU <101114124+BIIYEUDAU@users.noreply.github.com>
2022-03-07 19:58:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Nhiều lúc anh thấy thương em lắm…😓bởi vì chẳng thể bên cạnh em lúc em buồn🥺.Anh cũng cảm thấy mình chẳng có gì tốt cả, đến nỗi chưa lo được cho bản than mình nhưng lại muốn lo cho em rồi…🥺Anh thật sự may mắn khi có em. Nhưng mà đừng lo😙!! Dù trời có sập anh cũng gánh thay em😊. Hỏng ở đâu, anh sửa cùng em. Nếu buộc phải sai anh sai cùng em😚. Không dám chắc về kết quả, nhưng anh sẽ không để mất em….không bao giờ❤️!!!!",
+    titleWeb: "Gửi babiiii",
     introTitle: 'bae babi',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Nhiều lúc anh thấy thương em lắm…😓bởi vì chẳng thể bên cạnh em lúc em buồn🥺.Anh cũng cảm thấy mình chẳng có gì tốt cả, đến nỗi chưa lo được cho bản than mình nhưng lại muốn lo cho em rồi…🥺Anh thật sự may mắn khi có em. Nhưng mà đừng lo😙!! Dù trời có sập anh cũng gánh thay em😊. Hỏng ở đâu, anh sửa cùng em. Nếu buộc phải sai anh sai cùng em😚. Không dám chắc về kết quả, nhưng anh sẽ không để mất em….không bao giờ❤️!!!!`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Yêu lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại em muốn đẻ cho anh 1 đàn con :))',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, Anh qua đón đi chơi nha.',
+    btnAccept: 'Dạ anh nhớ mang áo mưa nha:))',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
